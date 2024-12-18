4986e96191161bf2842163de82a892e3ec66dfa3
choprlak <lakshay.chopra@adidas.com>
2024-12-18 15:57:49 +0530

    random prod classNames

--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -6,10 +6,14 @@ import { StylesProvider, createGenerateClassName } from '@material-ui/core/style
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'ma'
+});
+
 export default () => {
     return (
       <div>
-        <StylesProvider>
+        <StylesProvider generateClassName={generateClassName}>
           <BrowserRouter>
             <Switch>
               <Route exact path="/pricing" component={Pricing} />
