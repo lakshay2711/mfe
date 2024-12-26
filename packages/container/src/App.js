ddeb2d81d3cb950f3239578f36497817714e1c09
choprlak <lakshay.chopra@adidas.com>
2024-12-26 18:11:32 +0530

    added dashboard

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,9 +1,14 @@
-import React, { lazy, Suspense, useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React, { lazy, Suspense, useState, useEffect } from "react";
+import { Router, Route, Switch, Redirect } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
+
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+
 // import MarketingApp from "./components/MarketingApp";
 // import AuthApp from './components/AuthApp';
 import Header from "./components/header/Header";
@@ -11,6 +16,7 @@ import Progress from "./components/Progress";
 
 const MarketingApp = lazy(() => import("./components/MarketingApp"));
 const AuthApp = lazy(() => import("./components/AuthApp"));
+const DashboardApp = lazy(() => import("./components/DashboardApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -19,8 +25,12 @@ const generateClassName = createGenerateClassName({
 export default () => {
   const [isSignedIn, setIsSignedIn] = useState(false);
 
+  useEffect(() => {
+    isSignedIn && history.push('/dashboard');
+  }, [isSignedIn]);
+
   return (
-    <BrowserRouter>
+    <Router history={history}>
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header isSignedIn={isSignedIn} onSignOut={() => setIsSignedIn(false)} />
@@ -29,6 +39,10 @@ export default () => {
               <Route path="/auth">
                 <AuthApp onSignIn={() => setIsSignedIn(true)} />
               </Route>
+              <Route path="/dashboard">
+                { !isSignedIn && <Redirect to="/" /> }
+                <DashboardApp />
+              </Route>
               <MarketingApp path="/">
                 <MarketingApp isSignedIn={isSignedIn} />
               </MarketingApp>
@@ -36,6 +50,6 @@ export default () => {
           </Suspense>
         </div>
       </StylesProvider>
-    </BrowserRouter>
+    </Router>
   );
 };
---
+++ packages/container/src/components/DashboardApp.js
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef } from "react";
+import { useHistory } from "react-router-dom";
+
+import { mount } from "dashboard/DashboardApp";
+
+export default () => {
+  const ref = useRef(null);
+
+  useEffect(() => {
+    mount(ref.current);
+  }, []);
+
+  return (
+    <div ref={ref} />
+  )
+};
